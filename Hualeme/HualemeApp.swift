9baff5fe0bdc163c59c21b991477c4de9ae9b2da
ChuankangZhu1 <144566903+ChuankangZhu1@users.noreply.github.com>
2026-07-02 18:22:42 -0400

    Initial Commit

---
+++ Hualeme/HualemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HualemeApp.swift
+//  Hualeme
+//
+//  Created by 少废话你GPA多少 on 2026-07-02.
+//
+
+import SwiftUI
+
+@main
+struct HualemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
